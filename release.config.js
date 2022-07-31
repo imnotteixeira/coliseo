014e1b9d14f3fdd7c702d6dbd7e356d02b50aa4c
Angelo Teixeira <imnotteixeira@gmail.com>
2022-07-31 13:07:47 +0200

    fix: Add CI/CD with auto npm publish

--- release.config.js
+++ release.config.js
@@ -7,6 +7,7 @@ module.exports = {
       ["@semantic-release/git", {
         "assets": ["package.json"],
         "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-      }]
+      }],
+      "@semantic-release/github"
     ]
 }
